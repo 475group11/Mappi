d67d208db0ab26bfe9033f49a89e55989cd342b6
Sam Crow <samcrow@uw.edu>
2017-04-20 19:46:08 -0700

    Created service and initial code

--- src/mappi_lidar_snapshot/src/mappi_lidar_snapshot_node.cpp
+++ src/mappi_lidar_snapshot/src/mappi_lidar_snapshot_node.cpp
@@ -2,14 +2,23 @@
 
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
+#include <mappi_msgs/Snapshot.h>
+
+typedef boost::function<bool (mappi_msgs::Snapshot::Request&, mappi_msgs::Snapshot::Response&)> handler_callback;
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "mappi_lidar_snapshot_node");
     ros::NodeHandle handle;
 
-    std::cout << "mappi_lidar_snapshot_node starting up\n";
+    ROS_INFO("mappi_lidar_snapshot_node starting up\n");
 
-    // Parameters
+    auto take_snapshot = [] (mappi_msgs::Snapshot::Request&, mappi_msgs::Snapshot::Response& response) {
+        ROS_INFO("Taking snapshot");
+        response.result = 0;
+        return true;
+    };
+
+    auto service = handle.advertiseService("snapshot", handler_callback(take_snapshot));
 
     ros::spin();
 
