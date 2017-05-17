c9d37b6cc74c128d7785cadb2f322a81103536b1
Sam Crow <samcrow@uw.edu>
2017-05-17 16:30:28 -0700

    Updated BNO055 driver to provide calibration information, changed BNO055 topic names

--- src/bno055_imu/src/bno055_node_common.cpp
+++ src/bno055_imu/src/bno055_node_common.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 #include <geometry_msgs/Quaternion.h>
+#include <bno055_msgs/Calibration.h>
 
 #include "bno055_node_common.h"
 
@@ -19,12 +20,14 @@ void run_bno055_node(ros::NodeHandle& handle, IMU::Transport& transport) {
     ROS_INFO_STREAM("Error flag " << sensor.error());
 
     // Advertise
-    auto publisher = handle.advertise<sensor_msgs::Imu>("bno055_i2c", 8);
+    auto publisher = handle.advertise<sensor_msgs::Imu>("imu", 8);
+    auto calibration_publisher = handle.advertise<bno055_msgs::Calibration>("imu_calibration", 8);
 
     // The last message that was sent
     sensor_msgs::Imu previous_message;
 
-    auto send_callback = [frame_id, publisher, &sensor, &previous_message] (const ros::TimerEvent&) {
+    auto send_callback = [frame_id, publisher, calibration_publisher, &sensor, &previous_message]
+        (const ros::TimerEvent&) {
         try {
 	    // Send IMU data
             sensor_msgs::Imu imu_data;
@@ -32,7 +35,7 @@ void run_bno055_node(ros::NodeHandle& handle, IMU::Transport& transport) {
             imu_data.header.frame_id = frame_id;
 
             imu_data.orientation = sensor.orientation();
-            // TODO: This includes gravity. Is this correct?
+            // This includes gravity. Assumed correct.
             imu_data.linear_acceleration = sensor.acceleration();
             imu_data.angular_velocity = sensor.angular_velocity();
 
@@ -41,6 +44,19 @@ void run_bno055_node(ros::NodeHandle& handle, IMU::Transport& transport) {
         } catch (std::exception& e) {
             ROS_WARN("Failed to read data from sensor: %s", e.what());
             publisher.publish(previous_message);
+        }
+        try {
+            // Send calibration
+            const auto imu_calibration = sensor.calibration_state();
+            bno055_msgs::Calibration calibration_message;
+            calibration_message.accelerometer_status = static_cast<std::uint8_t>(imu_calibration.accelerometer);
+            calibration_message.gyroscope_status = static_cast<std::uint8_t>(imu_calibration.gyroscope);
+            calibration_message.magnetometer_status = static_cast<std::uint8_t>(imu_calibration.magnetometer);
+            calibration_message.system_status = static_cast<std::uint8_t>(imu_calibration.system);
+
+            calibration_publisher.publish(calibration_message);
+        } catch (std::exception& e) {
+            ROS_WARN("Failed to read calibration status from sensor: %s", e.what());
         }
     };
 
