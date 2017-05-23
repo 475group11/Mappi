e81be7b376416c82a24a92d542801e8b4fbf8ee0
Dustin Werran <dustinwerran@gmail.com>
2017-05-22 18:12:47 -0700

    committing to pull (though I don't think there is anything to pull

--- src/data_source_control/src/source_control.py
+++ src/data_source_control/src/source_control.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 
-import rospy
-# Note that while a Bool would be the obvious choice of
-# type here, python does not support Bools, and I couldn't
-# find anything on ROS answers about how the Bool std_msg
-# defines truthiness in standard python types.                                      
-from std_msgs.msg import Char
+import rospy                                     
+from std_msgs.msg import Bool
 from sensor_msgs.msg import Imu
 
 # globals
@@ -22,11 +18,14 @@ def update_imu_pv(dead_reckoning_on):
       for val in range(3):
          imu_pva[1][val] = imu_pva[1][val] + 0.1*imu_pva[2][val]
    else:
-      # TODO: constrain PV to LIDAR data
+      for vector in range(2):
+         for val in range(3):
+            imu_pva[vector][val] = lidar_pva[vector][val]
 
-def dead_reckoning():
-   # TODO: determine dead_reckoning_on 
-   update_imu_pv(dead_reckoning_on)
+def dead_reckoning(dead_reckoning_on):
+   
+   
+   
 
 
 def imu_callback(msg):
@@ -38,18 +37,18 @@ def lidar_callback(msg):
    # TODO: transform?
 
 def control():
-   pub = rospy.Publisher('control_value', Char)
+   pub = rospy.Publisher('control_value', Bool)
    sub_imu = rospy.Subscriber('bno005_serial_node', Imu, imu_callback)
    # TODO: we need to decide the type of message(s) sent by the lidar update tool
    # sub_lidar = rospy.Subscriber('lidar_pva_node', , lidar_callback)
    rospy.init_node('source_control', anonymous=True)
    rate = rospy.Rate(10)
+   dead_reckoning_on = False  # initialize without dead_reckoning
    while not rospy.is_shutdown():
-      to_publish = 'n'
-      if dead_reckoning():
-         to_publish = 'y'
-      rospy.loginfo(to_publish)
-      pub.publish(to_publish)
+      dead_reckoning_on = dead_reckoning(dead_reckoning_on)
+      update_imu_pv(dead_reckoning_on)
+      rospy.loginfo(dead_reckoning_on)
+      pub.publish(dead_reckoning_on)
       rate.spin()  # unsure if this is needed
       rate.sleep()
 
