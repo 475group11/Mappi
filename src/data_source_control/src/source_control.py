81c5b676bfbfe6376eaeaf2a54d80c0400fdceab
Dustin Werran <dustinwerran@gmail.com>
2017-05-22 19:05:35 -0700

    built out dead reckoning function

--- src/data_source_control/src/source_control.py
+++ src/data_source_control/src/source_control.py
@@ -1,37 +1,49 @@
 #!/usr/bin/env python
 
+import math
 import rospy                                     
 from std_msgs.msg import Bool
 from sensor_msgs.msg import Imu
 
+# read-only fields
+FREQUENCY = 10.0  # refresh frequency of the system
+GRAV_ACCELERATION = 9.81
+THRESHOLD_ACTIVATE = 10.0  # ratio of lidar acc to imu acc to activate dr
+THRESHOLD_DEACTIVATE = 10.0  # ratio of " to " to deactivate dr
+
 # globals
 # TODO: We will need a way to transfrom the frames to match orientation
-imu_pva = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
-lidar_pva = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+_imu_pva = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+_lidar_pva = [[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
 
 def update_imu_pv(dead_reckoning_on):
-   # this should be called at 10Hz, which gives t = 0.1s
-   # approximate via last value of velocity; there might be a better way to do this
-   if dead_reckoning_on:
+   if dead_reckoning_on:  # approximate current location based on previous vectors
       for val in range(3):
-         imu_pva[0][val] = imu_pva[0][val] + 0.1*imu_pva[1][val] + 0.01*imu_pva[2][val]
+         _imu_pva[0][val] = _imu_pva[0][val] + 
+            (1.0/FREQUENCY)*_imu_pva[1][val] + (1.0/FREQUENCY)**2*_imu_pva[2][val]
       for val in range(3):
-         imu_pva[1][val] = imu_pva[1][val] + 0.1*imu_pva[2][val]
-   else:
+         _imu_pva[1][val] = _imu_pva[1][val] + (1.0/FREQUENCY)*_imu_pva[2][val]
+   else:  # set all the positions and velocities to what is given by the lidar
       for vector in range(2):
          for val in range(3):
-            imu_pva[vector][val] = lidar_pva[vector][val]
+            _imu_pva[vector][val] = _lidar_pva[vector][val]
 
-def dead_reckoning(dead_reckoning_on):
-   
-   
-   
-
+def dead_reckoning(dead_reckoning_active):
+   imu_a_mag = math.sqrt(_imu_pva[2][0]**2 + _imu_pva[2][1]**2 + _imu_pva[2][2]**2)
+   lidar_a_mag = math.sqrt(_lidar_pva[2][0]**2 + _lidar_pva[2][1]**2 + _lidar_pva[2][2]**2)
+   ratio = lidar_a_mag / imu_a_mag
+   if dead_reckoning_active:
+      if ratio > THRESHOLD_DEACTIVATE:
+         return False
+   else:
+      if ratio > THRESHOLD_ACTIVATE:
+         return True
 
 def imu_callback(msg):
    # TODO: transform?
    # adjust for acceleration due to gravity here to simplify operations later
-   imu_pva[2] = [msg.acceleration.x, msg.acceleration.y, msg.acceleration.z - 9.81]
+   _imu_pva[2] = [msg.acceleration.x, msg.acceleration.y,
+      msg.acceleration.z - GRAV_ACCELERATION]
 
 def lidar_callback(msg):
    # TODO: transform?
@@ -42,13 +54,13 @@ def control():
    # TODO: we need to decide the type of message(s) sent by the lidar update tool
    # sub_lidar = rospy.Subscriber('lidar_pva_node', , lidar_callback)
    rospy.init_node('source_control', anonymous=True)
-   rate = rospy.Rate(10)
-   dead_reckoning_on = False  # initialize without dead_reckoning
+   rate = rospy.Rate(int(FREQUENCY))
+   dead_reckoning_active = False  # initialize without dead_reckoning
    while not rospy.is_shutdown():
-      dead_reckoning_on = dead_reckoning(dead_reckoning_on)
-      update_imu_pv(dead_reckoning_on)
-      rospy.loginfo(dead_reckoning_on)
-      pub.publish(dead_reckoning_on)
+      dead_reckoning_active = dead_reckoning(dead_reckoning_active)
+      update_imu_pv(dead_reckoning_active)
+      rospy.loginfo(dead_reckoning_active)
+      pub.publish(dead_reckoning_active)
       rate.spin()  # unsure if this is needed
       rate.sleep()
 
